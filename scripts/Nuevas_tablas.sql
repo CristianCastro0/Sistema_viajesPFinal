8893bdabede288c1af4fc2df212614238a698288
BrayanF-dev <briianfuentes2017@gmail.com>
2025-05-26 14:15:42 -0500

    Update Nuevas_tablas.sql
    Se crea la tabla auditoria_estado_tarjeta para poder realizar las respectivas consultas

--- scripts/Nuevas_tablas.sql
+++ scripts/Nuevas_tablas.sql
@@ -1,1 +1,10 @@
 - Creación de nuevas tablas
+
+CREATE TABLE auditoria_estado_tarjeta (
+    id SERIAL PRIMARY KEY,
+    tarjeta_id INTEGER NOT NULL,
+    estado_anterior VARCHAR(50),
+    estado_nuevo VARCHAR(50),
+    fecha_cambio TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
