1c9b835173eb99bf8f4b85a972fc220fd083e9db
BrayanF-dev <briianfuentes2017@gmail.com>
2025-05-26 15:30:36 -0500

    Update Nuevas_tablas.sql
    Se agregan dos tablas adicionales para pdoer resolver el punto 3

--- scripts/Nuevas_tablas.sql
+++ scripts/Nuevas_tablas.sql
@@ -8,3 +8,17 @@ CREATE TABLE auditoria_estado_tarjeta (
     fecha_cambio TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE dispositivos (
+    dispositivo_id SERIAL PRIMARY KEY,
+    tipo VARCHAR(50) NOT NULL,
+    ubicacion VARCHAR(100)
+);
+
+CREATE TABLE dispositivo_validacion (
+    id SERIAL PRIMARY KEY,
+    viaje_id INTEGER NOT NULL,
+    dispositivo_id INTEGER NOT NULL,
+    fecha_validacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (viaje_id) REFERENCES viajes(viaje_id),
+    FOREIGN KEY (dispositivo_id) REFERENCES dispositivos(dispositivo_id)
+);
