4595009ebaf0d0a3769bffcdabc08ade36f49d47
CristianCastro0 <cccastelblanco@ucompensar.edu.co>
2025-05-26 17:10:42 -0500

    Update Nuevas_tablas.sql

--- scripts/Nuevas_tablas.sql
+++ scripts/Nuevas_tablas.sql
@@ -1,5 +1,5 @@
 - Creación de nuevas tablas
-
+-- tabla Tarjeta auditoria
 CREATE TABLE tarjeta_auditoria (
 auditoria_id SERIAL PRIMARY KEY,
 tarjeta_id INT NOT NULL REFERENCES tarjetas(tarjeta_id),
@@ -8,6 +8,15 @@ estado_nuevo VARCHAR(20),
 fecha_cambio DATE NOT NULL
 );
 
+--tabla promocion
+CREATE TABLE promociones (
+promocion_id SERIAL PRIMARY KEY,
+nombre VARCHAR(50),
+descripcion TEXT
+);
+ALTER TABLE recargas ADD COLUMN promocion_id INT REFERENCES promociones(promocion_id);
+
+--dispositivo
 CREATE TABLE dispositivos (
     dispositivo_id SERIAL PRIMARY KEY,
     tipo VARCHAR(50) NOT NULL,
