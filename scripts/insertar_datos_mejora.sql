dbe92b1f0bae002469d9e941e726e5b74284fc8c
CristianCastro0 <cccastelblanco@ucompensar.edu.co>
2025-05-26 17:16:27 -0500

    Update insertar_datos_mejora.sql

--- scripts/insertar_datos_mejora.sql
+++ scripts/insertar_datos_mejora.sql
@@ -1,1 +1,8 @@
-
+-- insertamos datos de ejemplo
+INSERT INTO retroalimentacion(viaje_id, usuario_id, rating, comentario, fecha_envio) VALUES
+  (1, 1, 5, 'Excelente servicio',       '2025-04-02'),
+  (2, 2, 3, 'Regular, un poco lento',   '2025-04-03'),
+  (3, 1, 4, 'Buen viaje',               '2025-04-04'),
+  (4, 3, 2, 'Mala atención, queja',     '2025-04-05'),
+  (5, 2, 5, 'Muy rápido y cómodo',      '2025-04-06'),
+  (6, 1, 4, 'Buen precio',              '2025-04-07');
